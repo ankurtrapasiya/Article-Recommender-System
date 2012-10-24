e27567642cf6b4fcc67f6653275da4d0e92b93ea
ankur.trapasiya <ankur.trapasiya@gmail.com>
2012-10-24 20:03:50 +0530

    InterestDAOImpl Code Complete. Before Testing.

--- src/java/com/surpriseme/DAOImpl/InterestDAOImpl.java
+++ src/java/com/surpriseme/DAOImpl/InterestDAOImpl.java
@@ -6,9 +6,18 @@ package com.surpriseme.DAOImpl;
 
 import com.surpriseme.DAO.InterestDAO;
 import com.surpriseme.entities.Interest;
+import com.surpriseme.entities.User;
+import com.surpriseme.utils.DBConnection;
+import com.surpriseme.utils.Utilities;
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import org.apache.log4j.Logger;
+import org.apache.log4j.Priority;
 
 /**
  *
@@ -16,36 +25,256 @@ import java.util.List;
  */
 public class InterestDAOImpl implements InterestDAO {
 
+    CallableStatement cstmt;
+    DBConnection con;
+    private static final Logger logger = Logger.getLogger(UserDAOImpl.class);
+
     @Override
     public boolean saveOrUpdate(Interest entity) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet.");
+
+        ResultSet rs = null;
+        boolean retval = false;
+
+        try {
+
+            con = new DBConnection();
+            if (con.connect()) {
+
+                if (entity.getInterestid() != null) {
+
+                    cstmt = (CallableStatement) con.getConnection().prepareCall("{call sp_upd_interest(?,?,?,?)}");
+                    cstmt.setInt("p_interestid", entity.getInterestid());
+
+                } else {
+                    cstmt = (CallableStatement) con.getConnection().prepareCall("{call sp_ins_interest(?,?,?,?,?)}");
+                }
+
+
+                cstmt.setString("p_name", entity.getName());
+                cstmt.setString("p_description", entity.getDescription());
+                cstmt.setString("p_iconpath", entity.getIconpath());
+                cstmt.setTimestamp("p_timestamp", entity.getTimestamp());
+
+                rs = con.saveOrUpdate(cstmt);
+
+            }
+
+            retval = true;
+
+        } catch (ClassNotFoundException ex) {
+            logger.log(Priority.ERROR, ex.toString());
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            cstmt.close();
+            con.disconnect();
+        }
+
+        return retval;
+
     }
 
     @Override
     public void saveOrUpdateAll(List<Interest> entities) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet.");
+
+        ResultSet rs = null;
+
+        try {
+
+            con = new DBConnection();
+            if (con.connect()) {
+
+                Iterator<Interest> iterator = entities.iterator();
+
+                while (iterator.hasNext()) {
+
+                    Interest entity = iterator.next();
+
+                    if (entity.getInterestid() != null) {
+
+                        cstmt = (CallableStatement) con.getConnection().prepareCall("{call sp_upd_interest(?,?,?,?,?)}");
+                        cstmt.setInt("p_interestid", entity.getInterestid());
+
+                    } else {
+                        cstmt = (CallableStatement) con.getConnection().prepareCall("{call sp_ins_interest(?,?,?,?)}");
+                    }
+
+                    cstmt.setString("p_name", entity.getName());
+                    cstmt.setString("p_description", entity.getDescription());
+                    cstmt.setString("p_iconpath", entity.getIconpath());
+                    cstmt.setTimestamp("p_timestamp", entity.getTimestamp());
+
+                    rs = con.saveOrUpdate(cstmt);
+                }
+
+            }
+
+        } catch (ClassNotFoundException ex) {
+            logger.log(Priority.ERROR, ex.toString());
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            cstmt.close();
+            con.disconnect();
+        }
+
     }
 
     @Override
     public Interest findById(Integer key) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Interest retval = null;
+        ResultSet rs = null;
+        PreparedStatement pstmt = null;
+
+        try {
+
+            con = new DBConnection();
+
+            if (con.connect()) {
+
+                String sql = "select * from interest where interestid=?";
+                pstmt = con.getConnection().prepareStatement(sql);
+                pstmt.setInt(1, key);
+
+                rs = con.customQuery(pstmt);
+
+                while (rs.next()) {
+
+                    Interest interest = new Interest();
+
+                    interest.setInterestid(rs.getInt("interestid"));
+                    interest.setName(rs.getString("name"));
+                    interest.setDescription(rs.getString("description"));
+                    interest.setIconpath(rs.getString("iconpath"));
+                    interest.setTimestamp(rs.getTimestamp("timestamp"));
+
+                    retval = interest;
+                }
+
+            }
+
+        } catch (ClassNotFoundException ex) {
+            logger.log(Priority.ERROR, ex.toString());
+        } catch (SQLException ex) {
+            throw ex;
+        } finally {
+            con.disconnect();
+        }
+
+        return retval;
     }
 
     @Override
     public List<Interest> getAll() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet.");
+
+        List<Interest> retval = new ArrayList<Interest>();
+        ResultSet rs = null;
+
+        try {
+
+            con = new DBConnection();
+
+            if (con.connect()) {
+
+                cstmt = (CallableStatement) con.getConnection().prepareCall("{call sp_sel_interest()}");
+
+                rs = con.customQuery(cstmt);
+
+                while (rs.next()) {
+
+                    Interest interest = new Interest();
+
+                    interest.setInterestid(rs.getInt("interestid"));
+                    interest.setName(rs.getString("name"));
+                    interest.setDescription(rs.getString("description"));
+                    interest.setIconpath(rs.getString("iconpath"));
+                    interest.setTimestamp(rs.getTimestamp("timestamp"));
+
+
+                    retval.add(interest);
+                }
+
+            }
+
+        } catch (ClassNotFoundException ex) {
+            logger.log(Priority.ERROR, ex.toString());
+        } catch (SQLException ex) {
+            throw ex;
+        } finally {
+            con.disconnect();
+        }
+
+        return retval;
     }
 
     @Override
     public boolean delete(Integer key) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        boolean retval = false;
+        ResultSet rs = null;
+
+
+        try {
+
+            con = new DBConnection();
+
+            if (con.connect()) {
+
+                cstmt = (CallableStatement) con.getConnection().prepareCall("{call sp_del_interest(?)}");
+
+                cstmt.setInt(1, key);
+
+                rs = con.customQuery(cstmt);
+
+            }
+
+            retval = true;
+
+        } catch (ClassNotFoundException ex) {
+            logger.log(Priority.ERROR, ex.toString());
+        } catch (SQLException ex) {
+            throw ex;
+        } finally {
+            con.disconnect();
+        }
+
+        return retval;
     }
 
     @Override
     public boolean deleteAll(List<Interest> entities) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+        boolean retval = false;
 
-  
-   
+        try {
+
+            con = new DBConnection();
+
+            if (con.connect()) {
+
+
+                Iterator<Interest> iterator = entities.iterator();
+
+                while (iterator.hasNext()) {
+
+                    Interest entity = iterator.next();
+
+                    cstmt = (CallableStatement) con.getConnection().prepareCall("{call sp_del_interest(?)}");
+                    cstmt.setInt(1, entity.getInterestid());
+
+                    con.customQuery(cstmt);
+
+                }
+
+            }
+
+            retval = true;
+
+        } catch (ClassNotFoundException ex) {
+            logger.log(Priority.ERROR, ex.toString());
+        } catch (SQLException ex) {
+            throw ex;
+        } finally {
+            con.disconnect();
+        }
+        return retval;
+    }
 }
