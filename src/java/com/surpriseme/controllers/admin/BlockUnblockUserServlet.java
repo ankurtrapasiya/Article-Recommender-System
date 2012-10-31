ad7ed833d5b72dc154540b0558e9921f02d1f06b
Updateankur.trapasiya <ankur.trapasiya@gmail.com>
2012-10-31 20:28:15 +0530

    Added block user to web.xml

--- src/java/com/surpriseme/controllers/admin/BlockUnblockUserServlet.java
+++ src/java/com/surpriseme/controllers/admin/BlockUnblockUserServlet.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.surpriseme.controllers;
+package com.surpriseme.controllers.admin;
 
 import com.surpriseme.DAOImpl.UserDAOImpl;
 import com.surpriseme.entities.BlockedUsers;
